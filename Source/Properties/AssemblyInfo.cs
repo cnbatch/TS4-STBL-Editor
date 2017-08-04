248ad3b2753ee4bc4ddbe3336ddf1b7e6807fae8
ArtUrlWWW <qqq@qqq.qq>
2017-08-04 23:42:42 +0300

    Bump version to 14.0

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.1.0.0")]
-[assembly: AssemblyFileVersion("12.1.0.0")]
+[assembly: AssemblyVersion("14.0.0.0")]
+[assembly: AssemblyFileVersion("14.0.0.0")]

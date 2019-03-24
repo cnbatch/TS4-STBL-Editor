33dd1c2ad726a785e597f99a8cc44a5590ebd21d
ArtUrlWWW <khartnjava@gmail.com>
2019-03-24 22:42:47 +0300

    Added more readable STBL names in list

--- Source/SelectSTBLfileFromPackage.cs
+++ Source/SelectSTBLfileFromPackage.cs
@@ -31,7 +31,63 @@ namespace TS4_STBL_Editor
                 IResource res = WrapperDealer.GetResource(0, imppkg, rie, true);
                 BigInteger bi = BigInteger.Parse(rie.Instance.ToString());
 
-                String fileName = "0x" + bi.ToString("X");
+                String fileName = "0x" + (bi.ToString("X").Length < 16 ? "0" + bi.ToString("X") : bi.ToString("X"));
+
+                String fileNameLang = fileName.Substring(0, 4);
+                switch (fileNameLang)
+                {
+                    case "0x00":
+                        fileName = "ENG_US " + fileName;
+                        break;
+                    case "0x02":
+                        fileName = "CHT_CN " + fileName;
+                        break;
+                    case "0x03":
+                        fileName = "CZE_CZ " + fileName;
+                        break;
+                    case "0x04":
+                        fileName = "DAN_DK " + fileName;
+                        break;
+                    case "0x05":
+                        fileName = "DUT_NL " + fileName;
+                        break;
+                    case "0x06":
+                        fileName = "FIN_FI " + fileName;
+                        break;
+                    case "0x07":
+                        fileName = "FRE_FR " + fileName;
+                        break;
+                    case "0x08":
+                        fileName = "GER_DE " + fileName;
+                        break;
+                    case "0x0B":
+                        fileName = "ITA_IT " + fileName;
+                        break;
+                    case "0x0C":
+                        fileName = "JPN_JP " + fileName;
+                        break;
+                    case "0x0D":
+                        fileName = "KOR_KR " + fileName;
+                        break;
+                    case "0x0E":
+                        fileName = "NOR_NO " + fileName;
+                        break;
+                    case "0x0F":
+                        fileName = "POL_PL " + fileName;
+                        break;
+                    case "0x11":
+                        fileName = "POR_BR " + fileName;
+                        break;
+                    case "0x12":
+                        fileName = "RUS_RU " + fileName;
+                        break;
+                    case "0x13":
+                        fileName = "SPA_ES " + fileName;
+                        break;
+                    case "0x15":
+                        fileName = "SWE_SE " + fileName;
+                        break;
+                }
                 listBox1.Items.Add(fileName);
             }
         }
@@ -40,7 +96,8 @@ namespace TS4_STBL_Editor
         {
             foreach (string a in listBox1.SelectedItems)
             {
-                selectedSTBLObjects.Add(a);
+                String a1 = a.Substring(7);
+                selectedSTBLObjects.Add(a1);
             }
             this.Close();
         }
